d14595df6f6f12a32844f46df7ac6b58e14dac19
SergeyEdalov <edalov94@mail.ru>
2024-05-21 10:44:55 +0300

    Commit before changes

--- CheckUnputDataLibrary/obj/Debug/net8.0/CheckUnputDataLibrary.AssemblyInfo.cs
+++ CheckUnputDataLibrary/obj/Debug/net8.0/CheckUnputDataLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Geekbrains")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.1.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.1+ed358bf4f67a788f0759bfc2f0f4eca6802210d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.1+cb7519eb3371b20b8832abe79095e41715a63102")]
 [assembly: System.Reflection.AssemblyProductAttribute("CheckUnputDataLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CheckUnputDataLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.1.0")]
--- User/obj/Debug/net8.0/User.AssemblyInfo.cs
+++ User/obj/Debug/net8.0/User.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("User")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed358bf4f67a788f0759bfc2f0f4eca6802210d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb7519eb3371b20b8832abe79095e41715a63102")]
 [assembly: System.Reflection.AssemblyProductAttribute("User")]
 [assembly: System.Reflection.AssemblyTitleAttribute("User")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
