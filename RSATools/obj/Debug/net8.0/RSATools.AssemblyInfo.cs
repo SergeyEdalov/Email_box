66b2edcebba44882534df65762f19d13af159cb6
SergeyEdalov <edalov94@mail.ru>
2024-05-23 14:29:43 +0300

    Commit after merge

--- RSATools/obj/Debug/net8.0/RSATools.AssemblyInfo.cs
+++ RSATools/obj/Debug/net8.0/RSATools.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RSATools")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a6549bf2443664b0d31ae57c41d762a72168b3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d45f88343c56d1a75ec2e90f0364066c4d880db")]
 [assembly: System.Reflection.AssemblyProductAttribute("RSATools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RSATools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
